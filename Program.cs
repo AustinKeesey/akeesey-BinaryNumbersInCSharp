ba06da2ff0b88fcd1ce2412ee8063e0590fa2a30
AustinKeesey <akeesey2023@chadwickschool.org>
2022-01-12 12:27:51 -0800

    chore: Initialize project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace akeesey_BinaryNumbersInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
